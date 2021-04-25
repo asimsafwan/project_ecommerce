ede209b833bc521e6dd371e5ecad9acc02ea1ef8
Asim Safwan <asim.safwan@gmail.com>
2021-04-25 10:36:44 +0530

    Modifying and formatting existing files and also adding middlewares

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const mongoose = require("mongoose");
 const express = require("express");
-const app=express();
-mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true}).then(()=>{
-    console.log("DB CONNECTED")
-});
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const port=process.env.PORT||8000;
+const authRoutes = require("./routes/auth");
 
-app.listen(port,()=>{
-    console.log(`app is running at ${port}`)
+//DB Connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//My Routes
+app.use("/api", authRoutes);
+
+//PORT
+const port = process.env.PORT || 8000;
+
+//Starting a server
+app.listen(port, () => {
+  console.log(`app is running at ${port}`);
 });

a23a709606bcc0939b5366e8d01b97ccd4224c5c
Asim Safwan <asim.safwan@gmail.com>
2021-07-01 17:29:45 +0530

    Project Ecommerce Backend fixed and updated

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+//My routes
 const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
 
 //DB Connection
 mongoose
@@ -21,12 +26,16 @@ mongoose
   });
 
 //Middlewares
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 
 //My Routes
 app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", orderRoutes);
 
 //PORT
 const port = process.env.PORT || 8000;
